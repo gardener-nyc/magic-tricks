82c68d182e451ac568e8d3ff0331d8f2795e5286
Ian Hatcher-Williams <ian@gardener.nyc>
2020-03-19 17:46:44 -0400

    fixes.

--- src/centsToPrice.js
+++ src/centsToPrice.js
@@ -1,4 +1,4 @@
-centsToPrice = cents => {
+export default cents => {
 	const price = (cents / 100);
 	const parts = price.toFixed(2).split('.');
 	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
